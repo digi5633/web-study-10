f2f19a7172d4df5bb6831b0387fc4a3ff20c0d8d
digi5633 <digi5633@gmail.com>
2020-09-10 15:39:06 +0900

    이미지 저장하기

---
+++ src/web_study_10/controller/MultiUploadServlet.java
@@ -0,0 +1,78 @@
+package web_study_10.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+@WebServlet("/upload2.do")
+public class MultiUploadServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Process(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Process(request, response);
+	}
+
+	private void Process(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		if (request.getMethod().equalsIgnoreCase("GET")) {
+			System.out.println("GET");
+
+		} else {
+			System.out.println("POST");
+
+			response.setContentType("text/html; charset=UTF-8");
+
+			PrintWriter out = response.getWriter();
+			String savePath = "upload";
+			int uploadFileSizeLimit = 5 * 1024 * 1024;
+			String encType = "UTF-8";
+			
+			ServletContext context = getServletContext();
+			String uploadFilePath = context.getRealPath(savePath);
+			System.out.println("서버상의 실제 디렉토리 : ");
+			System.out.println(uploadFilePath);
+			
+			try {
+				MultipartRequest multi = new MultipartRequest(request,
+						uploadFilePath,
+						uploadFileSizeLimit,
+						encType,
+						new DefaultFileRenamePolicy());
+				Enumeration files = multi.getFileNames();
+				while (files.hasMoreElements()) {
+					String file = (String) files.nextElement();
+					String file_name = multi.getFilesystemName(file);
+					// 중복된 파일을 업로드할 경우 파일명이 바뀐다.
+					String ori_file_name = multi.getOriginalFileName(file);
+					out.print("<br> 업로드된 파일명 : " + file_name);
+					out.print("<br> 원본 파일명 : " + ori_file_name);
+					out.print("<hr>");
+				}
+
+			} catch (Exception e) {
+				System.out.println("예외 발생 : " + e);
+			}
+			
+		}
+
+	}
+
+}
